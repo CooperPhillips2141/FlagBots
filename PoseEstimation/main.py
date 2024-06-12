dfd5989bddfe57585e8f8d4fa525365a59b465c9
jwapico <altizer16@outlook.com>
2024-06-12 15:58:38 -0600

    add small delay to mirror command

--- PoseEstimation/main.py
+++ PoseEstimation/main.py
@@ -100,6 +100,8 @@ def mirror_command():
     while(time.time() - start_time < 60):
         # get left and right arm angles then send that data to the ardunio
         send_command(get_arm_angles())
+        # sleep for a quarter of a second
+        time.sleep(0.25)
     # return the arms back to down
     send_command(get_semaphore_angles('End of Word'))
 
